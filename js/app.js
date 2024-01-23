2a7ede2959f8bdf6eae6c586f809d1ec4fdaccff
Breno Moura <133166425+BrenoMoura00@users.noreply.github.com>
2024-01-22 22:53:05 -0300

    Update2 app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-let amigos =[];
+let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
+
     amigos.push(amigo.value);
 
     if (lista.textContent == '') {
@@ -14,25 +25,33 @@ function adicionar() {
     amigo.value = '';
 }
 
+function sortear() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
 
+    embaralhar(amigos);
 
-function sortear(){
-embaralha(amigos);
-
-}
-
-function embaralha(lista) {
-
-    function embaralhar(lista) {
-        for (let indice = lista.length; indice; indice--) {
-            const indiceAleatorio = Math.floor(Math.random() * indice);
-            [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
         }
     }
-    
 }
 
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
 
-function reniciar(){
-
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
